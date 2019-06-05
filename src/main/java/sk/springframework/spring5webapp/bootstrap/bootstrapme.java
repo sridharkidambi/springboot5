bde50f72e45d0c0d07192880944b441fb77faa16
SridharKidambi <sridharhere@gmail.com>
2019-06-05 10:22:15 +0530

    commandlinerunner configurations

--- src/main/java/sk/springframework/spring5webapp/bootstrap/bootstrapme.java
+++ src/main/java/sk/springframework/spring5webapp/bootstrap/bootstrapme.java
@@ -1,41 +1,41 @@
-package sk.springframework.spring5webapp.bootstrap;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.AutoConfigureOrder;
-import org.springframework.context.ApplicationEvent;
-import org.springframework.context.ApplicationListener;
-import org.springframework.context.event.ContextRefreshedEvent;
-import org.springframework.context.event.EventListener;
-import org.springframework.stereotype.Component;
-import sk.springframework.spring5webapp.model.author;
-import sk.springframework.spring5webapp.model.books;
-import sk.springframework.spring5webapp.repositories.AuthorInterface;
-import sk.springframework.spring5webapp.repositories.BooksInterface;
-
-import java.util.HashSet;
-
-@Component
-public class bootstrapme {
-
-    @Autowired
-    private AuthorInterface authorInterface;
-
-    @Autowired
-    private BooksInterface booksInterface;
-
-    @EventListener
-    public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
-        this.initme();
-    }
-
-    private void initme(){
-
-        author _sk=new author("Sridhar","kidambi");
-        books _book = new books("jave","Srping");
-        _sk.get_book().add(_book);
-
-
-        booksInterface.save(_book);
-        authorInterface.save(_sk);
-    }
-}
+//package sk.springframework.spring5webapp.bootstrap;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.autoconfigure.AutoConfigureOrder;
+//import org.springframework.context.ApplicationEvent;
+//import org.springframework.context.ApplicationListener;
+//import org.springframework.context.event.ContextRefreshedEvent;
+//import org.springframework.context.event.EventListener;
+//import org.springframework.stereotype.Component;
+//import sk.springframework.spring5webapp.model.author;
+//import sk.springframework.spring5webapp.model.books;
+//import sk.springframework.spring5webapp.repositories.AuthorInterface;
+//import sk.springframework.spring5webapp.repositories.BooksInterface;
+//
+//import java.util.HashSet;
+//
+//@Component
+//public class bootstrapme {
+//
+//    @Autowired
+//    private AuthorInterface authorInterface;
+//
+//    @Autowired
+//    private BooksInterface booksInterface;
+//
+//    @EventListener
+//    public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
+//        this.initme();
+//    }
+//
+//    private void initme(){
+//
+//        author _sk=new author("Sridhar","kidambi");
+//        books _book = new books("jave","Srping");
+//        _sk.get_book().add(_book);
+//
+//
+//        booksInterface.save(_book);
+//        authorInterface.save(_sk);
+//    }
+//}
--- src/main/java/sk/springframework/spring5webapp/customRunner.java
+++ src/main/java/sk/springframework/spring5webapp/customRunner.java
@@ -5,6 +5,10 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Component;
 import sk.springframework.spring5webapp.controller.booksController;
+import sk.springframework.spring5webapp.model.author;
+import sk.springframework.spring5webapp.model.books;
+import sk.springframework.spring5webapp.repositories.AuthorInterface;
+import sk.springframework.spring5webapp.repositories.BooksInterface;
 
 @Component
 public class customRunner implements CommandLineRunner {
@@ -12,10 +16,28 @@ public class customRunner implements CommandLineRunner {
     @Autowired
     private booksController booksController1;
 
+    @Autowired
+    private AuthorInterface authorInterface;
+
+    @Autowired
+    private BooksInterface booksInterface;
+
     @Override
     public void run(String... args) throws Exception {
         System.out.println("I am custom commadlieRunner");
+        this.initme();
         booksController1.getBooks(null);
 
+    }
+
+    private void initme(){
+
+        author _sk=new author("Sridhar","kidambi");
+        books _book = new books("jave","Srping");
+        _sk.get_book().add(_book);
+
+
+        booksInterface.save(_book);
+        authorInterface.save(_sk);
     }
 }
