324ffe88afb0b187a23f050f874b1fa79117d18f
SridharKidambi <sridharhere@gmail.com>
2019-06-05 13:58:48 +0530

    Environment variables usage.

--- src/main/java/sk/springframework/spring5webapp/config/propertyConfig.java
+++ src/main/java/sk/springframework/spring5webapp/config/propertyConfig.java
@@ -1,11 +1,14 @@
 package sk.springframework.spring5webapp.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.EnvironmentAware;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.env.Environment;
 
 import javax.annotation.Resource;
 
@@ -17,17 +20,21 @@ public class propertyConfig {
     private String familyName;
 
 
+    @Autowired
+    public Environment env;
+
+
     @Bean
     public String getFmName(){
 
-        System.out.println("familyName is "+ familyName);
+        System.out.println("familyName is "+ env.getProperty("family.name"));
         return familyName;
     }
 
-    @Bean
-    public  static PropertySourcesPlaceholderConfigurer properties(){
-        PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer=new PropertySourcesPlaceholderConfigurer();
-
-        return propertySourcesPlaceholderConfigurer;
-    }
+//    @Bean
+//    public  static PropertySourcesPlaceholderConfigurer properties(){
+//        PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer=new PropertySourcesPlaceholderConfigurer();
+//
+//        return propertySourcesPlaceholderConfigurer;
+//    }
 }
